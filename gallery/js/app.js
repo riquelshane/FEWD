5737ab5d8bf73e2fd2cc07c3d4502747a5c548c1
riquelshane <rqlshane88@gmail.com>
2015-06-22 19:09:09 -0700

    Completed Nav Homework 6/22

---
+++ gallery/js/app.js
@@ -0,0 +1,5 @@
+var sliderElement = document.getElementById('slider');
+sliderElement.Slider();
+
+var navElement = document.getElementById('nav');
+navElement.Nav();
---
+++ gallery/js/gallery.js
@@ -0,0 +1,37 @@
+//Define prototypical Gallery function
+Element.prototype.Gallery = function(){
+
+  // Define global variables
+
+  this.layoutPhotos = function(){
+      // add logic for each photo in here
+
+  };
+
+  this.connect = function(){
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", "./models/popular-photos.json", true);
+      xhr.setRequestHeader("Content-Type", "application/json");
+
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+
+          // JSON.parse does not evaluate the attacker's scripts via xhr.responseText.
+
+        }
+      }
+      xhr.send();
+  };
+
+  this.init = function(){
+
+    this.connect();
+
+  };
+
+
+  this.init(); // do tasks on initialization.
+
+
+};
+/* end Gallery */
