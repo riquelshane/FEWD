895e828a6268b79679f06c6f1a02bdf844342bd4
riquelshane <rqlshane88@gmail.com>
2015-06-30 09:53:32 -0700

    added search bar.

--- gallery/js/search.js
+++ gallery/js/search.js
@@ -1,11 +1,22 @@
-Element.prototype.Search = function(){
-  var gallery = document.getElementById('gallery');
-
-
-
-  //when the user focuses on the input, clear its contents
-  //after the user presses ENTER/RETURN, filter the gallery <li> using tags from the JSON model.
-// li.dataset.description = photo.description;
-// set the tags as data tribues
-//data tags
-};
+// Element.prototype.Search = function(){
+//   var gallery = document.getElementById('gallery');
+//   var search = document.getElementById("ToolSearchField").value;
+//       search.remove();
+// };
+//
+//   //when the user focuses on the input, clear its contents
+//   //after the user presses ENTER/RETURN, filter the gallery <li> using tags from the JSON model.
+// // li.dataset.description = photo.description;
+// // set the tags as data tribues
+// //data tags
+//
+// for(var i=0; i<navItems.length; i++) {
+//   navItems[i].addEventListener('click', function(ev){
+//
+//     var id = ev.target.dataset.section;
+//     nav.showSection(id);
+//     nav.toggleNav();
+//
+//
+//   });
+// }
